0a84f91dbd5506902ad0f09c130d7cefc39fcf4f
NunoPress LLC <hello@nunopress.com>
2014-12-05 19:25:03 +0100

    Release v1.0.9
    Optimizations and rewrite config/headers variables, please check the
    README.md for more informations.

--- jscomments.php
+++ jscomments.php
@@ -1,15 +1,15 @@
-<?php
-namespace Grav\Plugin;
+<?php namespace Grav\Plugin;
 
+use Grav\Common\Page\Page;
 use Grav\Common\Plugin;
 
 class JSCommentsPlugin extends Plugin
 {
+  private $provider;
+
   public static function getSubscribedEvents() {
     return [
-      'onPluginsInitialized'  => ['onPluginsInitialized', 0],
-      'onTwigTemplatePaths'   => ['onTwigTemplatePaths', 0],
-      'onPageInitialized'     => ['onPageInitialized', 0]
+      'onPluginsInitialized' => ['onPluginsInitialized', 0]
     ];
   }
 
@@ -17,48 +17,51 @@ class JSCommentsPlugin extends Plugin
   {
     if ( $this->isAdmin() ) {
       $this->active = false;
+      return;
     }
+
+    $this->enable([
+      'onTwigTemplatePaths'   => ['onTwigTemplatePaths', 0],
+      'onPageInitialized'     => ['onPageInitialized', 0]
+    ]);
   }
 
   public function onTwigTemplatePaths()
   {
-    if ( ! $this->active ) return;
-
     $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
   }
 
   public function onPageInitialized()
   {
-    if ( ! $this->active ) return;
+    $this->mergeConfig($this->grav['page']);
 
     /* save page object */
     $page = $this->grav['page'];
 
-    /* get defaults from config file */
-    $defaults = (array) $this->config->get('plugins.jscomments');
+    $options = $this->config->get('plugins.jscomments');
 
     /* define valid providers */
-    $providers = [
-      'disqus',
-      'intensedebate',
-      'facebook',
-      'muut'
-    ];
+    $providers = $options['providers'];
 
-    /* validate header */
+    if ( ! $this->validateProvider($options['provider']) ) {
+      return;
+    }
+
+    $this->provider = $options['provider'];
+  }
+
+  private function validateProvider( $provider )
+  {
+    $options = $this->config->get('plugins.jscomments');
+
+    return ( isset($options['provider']) && in_array($options['provider'], $options['providers']) ) ? true : false;
+  }
+
+  private function mergeConfig( Page $page )
+  {
+    $defaults = (array) $this->config->get('plugins.jscomments');
     if ( isset($page->header()->jscomments) ) {
-      /* validate provider */
-      if ( isset($page->header()->jscomments['provider']) and in_array($page->header()->jscomments['provider'], $providers) ) {
-        /* save provider */
-        $provider = $page->header()->jscomments['provider'];
-
-        /* merge config with header page */
-        $page->header()->jscomments = array_merge($defaults, $page->header()->jscomments);
-      } else {
-        return; // need pass throw
-      }
-    } else {
-      $page->header()->jscomments = $defaults;
+      $this->config->set('plugins.jscomments', array_merge($defaults, $page->header()->jscomments));
     }
   }
 }
